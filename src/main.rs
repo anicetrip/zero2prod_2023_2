e6ec17bed2e7564c87cff79b8676a60acd9447ee
xdryk <yin1232017@outlook.com>
2023-03-08 22:27:56 +0000

    finish chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{net::TcpListener, fmt::format};
 
+use sqlx::{PgConnection, Connection, PgPool};
 use zero2prod::configuration::get_configuration;
 use zero2prod::setup::run;
 
@@ -7,9 +8,10 @@ use zero2prod::setup::run;
 async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string()).await.expect("failed to connect to postgres.");
     let address = format!("127.0.0.1:{}",configuration.application_port);
     println!("{}",address);
     let listerner = TcpListener::bind(address)?;
-    run(listerner)?.await
+    run(listerner,connection_pool)?.await
 }
 
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,15 +1,17 @@
-use std::{net::TcpListener, fmt::format};
+use std::net::TcpListener;
+use sqlx::{Connection,Executor,PgConnection,PgPool};
+use uuid::Uuid;
+use zero2prod::{configuration::{get_configuration,DatabaseSetting}, setup::run};
 
-use sqlx::{Connection, PgConnection};
-use zero2prod::{setup::run, configuration::get_configuration};
+
 
 #[tokio::test]
 async fn health_check_works() {
-    let address = spawn_app();
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
 
     let response = client
-        .get(&format!("{}/health_check",&address))
+        .get(&format!("{}/health_check", &app.address))
         .send()
         .await
         .expect("failed to execute request");
@@ -18,82 +20,130 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
 
-
-
-
-
-fn spawn_app() -> String {
+async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = run(listener).expect("failed to bind address");
+    let address = format!("http://127.0.0.1:{}", port);
+
+    let mut  configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+    let connection_pool = configure_database(&configuration.database)
+        .await;
+    let server = run(listener, connection_pool.clone()).expect("failed to bind address");
     let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
+    TestApp {
+        address,
+        db_pool: connection_pool,
+    }
+}
+
+pub async fn configure_database(config: &DatabaseSetting) -> PgPool {
+    let mut connection=PgConnection::connect(
+        &config.connection_string_without_db()
+        )
+        .await
+        .expect("Failed to connect to Postgres");
+        connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#,config.database_name).as_str())
+        .await
+        .expect("Failed to create database.");
+        // Migrate database
+        let connection_pool=PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+        sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database ");
+        connection_pool
 }
 
 
 
-
 #[tokio::test]
-async fn subscribe_returns_a_200_for_valid_form_data(){
-    let app_address = spawn_app();
-    let configuration = get_configuration().expect("failed to read the configuration");
-    let connection_string = configuration.database.connection_string();
-    let mut connection = PgConnection::connect(&connection_string)
-        .await
-        .expect("Failed to connect to postgres");
-
-
-
-
-
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
-
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
-                                    .post(&format!("{}/subscriptions",&app_address))
-                                    .header("Content-Type", "application/x-www-form-urlencoded")
-                                    .body(body)
-                                    .send()
-                                    .await
-                                    .expect("failed to execute request");
-    assert_eq!(200,response.status().as_u16());             
-
-    let saved = sqlx::query!("SELECT email,name FROM subscriptions",)
-        .fetch_one(&mut connection)
+        .post(&format!("{}/subscriptions", &app.address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
         .await
-        .expect("Failed to fetch saved subscription");
+        .expect("Failed to execute request.");
 
-    assert_eq!(saved.email,"ursula_le_guin@gmail.com");
-    assert_eq!(saved.name,"le guin")
+    // Assert
+    assert_eq!(200, response.status().as_u16());
 
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
 
-
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
+// #[tokio::test]
+// async fn subscribe1_returns_a_200_for_valid_form_data() {
+//     let app = spawn_app().await;
+//     let configuration = get_configuration().expect("failed to read the configuration ");
+//     let connection_string = configuration.database.connection_string();
+//     let mut connection = PgConnection::connect(&connection_string)
+//         .await
+//         .expect("Failed to connect to postgres ");
 
+//     let client = reqwest::Client::new();
+
+//     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+//     let response = client
+//         .post(&format!("{}/subscriptions", &app.address))
+//         .header("Content-Type", "application/x-www-form-urlencoded")
+//         .body(body)
+//         .send()
+//         .await
+//         .expect("failed to execute request");
+//     assert_eq!(200, response.status().as_u16());
+
+//     let saved = sqlx::query!("SELECT email,name FROM subscriptions",)
+//         .fetch_one(&mut connection)
+//         .await
+//         .expect("Failed to fetch saved subscription");
+
+//     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+//     assert_eq!(saved.name, "le guin")
+// }
 
 #[tokio::test]
-async fn subscribe_returns_a_400_when_data_is_missing(){
-    let app_address = spawn_app();
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
     let test_case = vec![
-        ("name=le%20guin","missing the email"),
-        ("email=ursula_le_guin%40gmail.com","missing the name"),
-        ("","missing both name and email")
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
     ];
 
-    for (invalid_body,error_message) in test_case {
+    for (invalid_body, error_message) in test_case {
         let response = client
-                .post(&format!("{}/subscriptions",&app_address))
-                .header("Content-Type","application/x-www-form-urlencoded")
-                .body(invalid_body)
-                .send()
-                .await
-                .expect("failed to execute request");
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("failed to execute request");
 
-        assert_eq!(400,response.status().as_u16(),"The api did not fail with 400 bad request when the payload was {}", error_message);        
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The api did not fail with 400 bad request when the payload was {}",
+            error_message
+        );
     }
-
-
 }
