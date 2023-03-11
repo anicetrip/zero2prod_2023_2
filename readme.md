c373c86940ff60092b92cfa5cb9b0f23fb361782
xdryk <yin1232017@outlook.com>
2023-03-11 02:24:41 +0000

    change time limit fro db link in docker

--- readme.md
+++ readme.md
@@ -5,3 +5,7 @@
 http://127.0.0.1:8000/subscriptions`
 
 `TEST_LOG=true cargo test health_check_works|bunyan`
+
+`docker build --tag zero2prod --file Dockerfile .`
+
+`docker run -p 8000:8000 zero2prod`
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
 use sqlx::PgPool;
@@ -11,14 +12,19 @@ async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool =
-        PgPool::connect(configuration.database.connection_string().expose_secret())
-            .await
-            .expect("failed to connect to postgres.");
-        let address=format!(
-            "{}:{}",
-            configuration.application.host,configuration.application.port
-            );
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
+        .expect("Failed to create Postgres connection pool.");
+
+    // let connection_pool =
+    //     PgPool::connect(configuration.database.connection_string().expose_secret())
+    //         .await
+    //         .expect("failed to connect to postgres.");
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     println!("{}", address);
     let listerner = TcpListener::bind(address)?;
     run(listerner, connection_pool)?.await
